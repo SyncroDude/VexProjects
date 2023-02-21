4697835a65d440c53b57b851f9f22bd40d6c7c5a
Shea Stanley <sheahstanley@comcast.net>
2023-02-21 13:42:42 -0500

    holeFinder Update
    I've continued progress on the hole finder function. It can now determine if a distance between two coordinates is larger than the robot's width. This can be used in conjunction with the scan function to re-scan an area to determine if a gap is present.

--- Python/MazeSolver_Python_V01/src/main.py
+++ Python/MazeSolver_Python_V01/src/main.py
@@ -26,10 +26,11 @@
 # drivetrain.turn_for(RIGHT, 89, DEGREES, wait=True)
 
 #import Vex Library
-from vex import *
+from vex import * 
+import math 
 
 # Global Varables
-global a,i,vehicleWidth,startingPoint,scanAreaOutput,scanSensorValue,currentPosition,desiredNode,positionNodes,holeFinderInput, dictA, dictB, dictXa, dictYa, dictXb, dictYb
+global a,i,vehicleWidth,startingPoint,scanAreaOutput,scanSensorValue,currentPosition,desiredNode,positionNodes,holeFinderInput, dictA, dictB, dictXa, dictYa, dictXb, dictYb, wallLength, wallLengthLarger, coordinateSet
 
 vehicleWidth = 100 #Units in milimeters
 startingPoint =[0,0]
@@ -40,7 +41,7 @@ def scanArea(a): #return list of distances from scanning point given a selected 
         scanAreaOutput = []
         i = 0
         # Scan a given range and add the variables to an array twenty times
-        for i-1 in range(19){
+        for i-1 in range(19):
             # Turning and scanning
             DriveTrain.turn_for(RIGHT, a/20, DEGREES, wait=True)
             scanSensorValue = front_distance.get_distance(MM)
@@ -49,33 +50,51 @@ def scanArea(a): #return list of distances from scanning point given a selected 
             scannedPoint = {
                 "angle": i*(a/20),
                 "distance": scanSensorValue,
-                "xCoordinate": (front_distance.get_distance(MM)+150) * math.cos(i*(a/20)/ 180.0 * math.pi),
-                "yCoordinate": (front_distance.get_distance(MM)+150) * math.sin(i*(a/20)/ 180.0 * math.pi),
+                "xCoordinate": (scanSensorValue+150) * math.cos(i*(a/20)/ 180.0 * math.pi),
+                "yCoordinate": (scanSensorValue+150) * math.sin(i*(a/20)/ 180.0 * math.pi),
             }
-        # Add the dictionary item to an array for output
-        append.scanAreaOutput(scannedPoint)
-        # Add to variable until condition is met
-        i=i++
-        }
+            # Add the dictionary item to an array for output
+            scanAreaOutput.append(scannedPoint)
+            # Add to variable until condition is met
+            i=i++
+        
         # Return the completed array
         return scanAreaOutput
 
 def holeFinder(holeFinderInput): #Input array of dictionary inputs
       i = 0
+      wallLength = 0 
+      wallLengthLarger = []
+      coordinateSet = []
       # Take all scanned points
-      for i in holeFinderInput.length():
-        dictA=holeFinderInput[i]  
-        dictB=holeFinderInput[i-1]
-        dictXa= dictA["xCoordinate"] #scannedPoint["xCoordinate"]
-        dictYa= dictA["yCoordinate"] #scannedPoint["yCoordinate"]
-        dictXb= dictB["xCoordinate"] #scannedPoint["xCoordinate"]
-        dictYb= dictB["yCoordinate"] #scannedPoint["yCoordinate"]
+      for i in holeFinderInput.length():{
+        dictA=holeFinderInput[i];  
+        dictB=holeFinderInput[i-1];
+        dictXa= dictA["xCoordinate"]; #scannedPoint["xCoordinate"]
+        dictYa= dictA["yCoordinate"]; #scannedPoint["yCoordinate"]
+        dictXb= dictB["xCoordinate"]; #scannedPoint["xCoordinate"]
+        dictYb= dictB["yCoordinate"]; #scannedPoint["yCoordinate"]
+        # Draw walls for each set of coordinates
+        wallLength = (dictXb-dictXa)/(dictYb-dictYa)
+        # Take the walls and measure the distance of said wall. If the wall is long enough, add it to an array to rescan. 
+        if (wallLength > (vehicleWidth + 100 ) ){
+            coordinateSet = [1,dictXa,dictYa,dictXb,dictYb];
+            wallLengthLarger.append(coordinateSet);
+        else{
+            wallLengthLarger.append(None);
+        }
+        }
+      return wallLengthLarger
+    }
+
+
       # Create virtual walls between each point. This doesn't neccisarily mean that a wall actually exists there, the robot will assume it is. 
       # The lines must be built consecutively or else this won't make sense. Walls must be created from point 0 to 1, 1 to 2, 2 to 3, ect.
       # If a point on a wall is too long between two points, output the angles between those two points for rescan.
       # If hole finder does not find a hole, return none
 
 def nodeCreator():
+    # Make triangles from the origin to build walls
       # Somehow find an ideal spot for a node to be
       # Mark its coordinates relative to the origin
       # If the node is in the same region as another node, do not create a new node.
