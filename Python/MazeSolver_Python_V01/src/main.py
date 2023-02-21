0c4d4b84c0f6bc9afa4e204f417d791e494ead58
Shea Stanley <sheahstanley@comcast.net>
2023-02-21 04:45:46 +0000

    Gameplan
    I commented out a bunch of code as well as added a few functions to break down my idea. I'm going to try to work on one function per day so I can check off each one as I go. If I need to go back and edit a function that's fine, as there's a chance my prior code won't meet the requirements of the future.

--- Python/MazeSolver_Python_V01/src/main.py
+++ Python/MazeSolver_Python_V01/src/main.py
@@ -15,11 +15,11 @@
 # No goal contition is included as the maze does not have any specifc conditions that would communicate a completed maze.
 
 # Functions to be included
-# Scan Function
-# High Resolution scan (could combine with the last one, possibly make the function scan between two given angles)
-# Create wall structure using datapoints to calculate possible holes in walls. If an area needs higher resolution, rescan that specific area and recreate the calculation to include said area. 
-# Create ideal node that the robot can drive to, and assumes that a possible path to an exit is present. Output the coordinates of this node relative to the starting point. 
-# Return to Node of given variable
+# Scan Function DONE
+# High Resolution scan (could combine with the last one, possibly make the function scan between two given angles) Nearly Done
+# Create wall structure using datapoints to calculate possible holes in walls. If an area needs higher resolution, rescan that specific area and recreate the calculation to include said area. Make sure to run this function after each scan. In progress
+# Create ideal node that the robot can drive to, and assumes that a possible path to an exit is present. Output the coordinates of this node relative to the starting point. In Progress
+# Return to Node of given variable In Progress
 
 # drivetrain.drive_for(FORWARD, 3000, MM, wait=True)
 
@@ -29,32 +29,57 @@
 from vex import *
 
 # Global Varables
-global a,i,vehicleWidth,startingPoint,scanAreaOutput
+global a,i,vehicleWidth,startingPoint,scanAreaOutput,scanSensorValue,currentPosition,desiredNode,positionNodes
 
 vehicleWidth = 100 #Units in milimeters
 startingPoint =[0,0]
 
 def scanArea(a): #return list of distances from scanning point given a selected angle range to scan
         i = 0
+        
         scanAreaOutput = []
+        # Scan a given range and add the variables to an array twenty times
         for i-1 in range(19){
+            # Turning and scanning
             DriveTrain.turn_for(RIGHT, a/20, DEGREES, wait=True)
+            scanSensorValue = front_distance.get_distance(MM)
+            # Create a dictionary to assign values for easy access. Just clarify what value you want from stating the string (text) associated with each variable
+            # FIX X AND Y COORDINATES TO BE BASED OFF ORIGIN NOT BASED OFF CURRENT POSITION
             scannedPoint = {
                 "angle": i*(a/20),
-                "distance": front_distance.get_distance(MM),
+                "distance": scanSensorValue,
                 "xCoordinate": (front_distance.get_distance(MM)+150) * math.cos(i*(a/20)/ 180.0 * math.pi),
                 "yCoordinate": (front_distance.get_distance(MM)+150) * math.sin(i*(a/20)/ 180.0 * math.pi),
             }
+        # Add the dictionary item to an array for output
         append.scanAreaOutput(scannedPoint)
+        # Add to variable until condition is met
         i=i++
         }
+        # Return the completed array
         return scanAreaOutput
 
+def holeFinder():
+      # Take all scanned points
+      # Create virtual walls between each point. This doesn't neccisarily mean that a wall actually exists there, the robot will assume it is. 
+      # The lines must be built consecutively or else this won't make sense. Walls must be created from point 0 to 1, 1 to 2, 2 to 3, ect.
+      # If a point on a wall is too long between two points, output the angles between those two points for rescan.
+      # If hole finder does not find a hole, return none
 
+def nodeCreator():
+      # Somehow find an ideal spot for a node to be
+      # Mark its coordinates relative to the origin
 
+def driveToNode(desiredNode):
+      # Take Current Position
+      # Find out how many nodes away the desired node is based off current location
+      # Plan out a path to each node
+      # Drive the path until the robot has reached its destination
+
+    
 def when_started1():
 # Brain should be defined by default
-positionNodes = [] #creates a list for collecting position nodes.
+    positionNodes = [] #creates a list for collecting position nodes.
 #positionNodes.append adds item to the end of the list. 
 
 
