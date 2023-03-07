1919493480415aa2870cbb4415604156c0d21d27
Shea Stanley <sheahstanley@comcast.net>
2023-03-07 14:10:14 -0500

    Testing in Class
    Fixed some errors and found some issues I need to solve. 
    1. Account for the width of the Vehicle
    2. Angle the boom arm so it doesn't get caught on the floor
    3. Add and edge case for non-90 degree turns.

--- Python/MazeSolver_Python_V02/src/main.py
+++ Python/MazeSolver_Python_V02/src/main.py
@@ -15,8 +15,8 @@ from vex import *
 brain=Brain()
 
 # Robot configuration code
-left_drive_smart = Motor(Ports.PORT10, GearSetting.RATIO_18_1, False)
-right_drive_smart = Motor(Ports.PORT1, GearSetting.RATIO_18_1, True)
+left_drive_smart = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+right_drive_smart = Motor(Ports.PORT10, GearSetting.RATIO_18_1, True)
 drivetrain_inertial = Inertial(Ports.PORT5)
 drivetrain = SmartDrive(left_drive_smart, right_drive_smart, drivetrain_inertial, 319.19, 320, 40, MM, 1)
 range_finder_a = Sonar(brain.three_wire_port.a)
@@ -37,13 +37,15 @@ def calibrate_drivetrain():
     brain.screen.set_cursor(1, 1)
 
 def when_started1():
-    global sensingDistance, drivingDistance, totalTravel, TravelLength,
+    global sensingDistance, drivingDistance, totalTravel, TravelLength
     drivingDistance = 0
     totalTravel = [] # For the totalTravel array, 0 means driving, 1 means turning (negative value represents turning left, while a positive value representss turing right), 2 means decision point
     while (True == True):
       sensingDistance = range_finder_a.distance(MM)
+      brain.screen.print(sensingDistance)
+      brain.screen.next_row()
       drivingDistance = 0
-      if(sensingDistance > 50):
+      if(sensingDistance > 150 or drivingDistance > 200):
           drivetrain.drive(FORWARD)
           drivingDistance += 1
       else:
@@ -57,17 +59,17 @@ def when_started1():
           drivetrain.turn_for(LEFT, 180, DEGREES)
           optionB = range_finder_a.distance(MM)
           drivetrain.turn_for(RIGHT, 90, DEGREES)
-          if(optionA>50 & optionB>50):
+          if(optionA>150 and optionB>150):
               drivetrain.turn_for(RIGHT, 90, DEGREES)
               addItem = [2,90]
               totalTravel.append(addItem)
               addItem.clear()
-          elif(optionA>50):
+          elif(optionA>150):
               drivetrain.turn_for(RIGHT, 90, DEGREES)
               addItem = [1,90]
               totalTravel.append(addItem)
               addItem.clear()
-          elif(optionB>50):
+          elif(optionB>150):
               drivetrain.turn_for(LEFT, 90, DEGREES)
               addItem = [1,-90]
               totalTravel.append(addItem)
@@ -75,7 +77,7 @@ def when_started1():
           else: #turn around and return to last decision point
               TravelLength = totalTravel.count()
               while (TravelLength != -1):
-                TravelItem = totalTravel(TravelLength)
+                TravelItem = totalTravel[TravelLength]
                 TravelIdentifier = TravelItem [0]
                 TravelValue = [1]
                 if (TravelIdentifier == 0 ):
@@ -91,9 +93,5 @@ def when_started1():
                     totalTravel.pop(TravelLength+1)
                     addItem.clear
                     break
-    if (brain.screen.pressed(callback) == True):
-        brain.screen.print("Stopping Program")
-        wait(3, SECONDS)
-        stop_project()
 
 when_started1() 
