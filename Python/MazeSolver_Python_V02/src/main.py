6114f0eb58dd26e30447173f8106be869a5bf422
Shea Stanley <sheahstanley@comcast.net>
2023-03-09 13:32:43 -0500

    Actually adding the wall scanning function
    Didn't save, here's the real push

--- Python/MazeSolver_Python_V02/src/main.py
+++ Python/MazeSolver_Python_V02/src/main.py
@@ -9,6 +9,7 @@
 
 # Library imports
 from vex import *
+import math
 
 # Brain should be defined by default
 # Brain should be defined by default
@@ -36,18 +37,74 @@ def calibrate_drivetrain():
     brain.screen.clear_screen()
     brain.screen.set_cursor(1, 1)
 
+def midpointDetectAndDrive():
+    # turn to the leftmost heading, then turn from the left until sensed distance is greater than 100. Once achiived, turn to the rightmost heading and do the same.
+    # Take the two distances collected, give them coodrdinates based on your current location (0,0), then interpret output coodrinates so the robot drives to the midpoint of that line.
+    # Version one, drive to the coordinates using components (easy)
+    # Version two, drive to the coodinates by taking the shortest path (harder, with more hurtles to overcome. How do I know the previous heading?)
+    
+    brain.screen.print("Collecting Data for midpoint scan")
+    brain.screen.next_row() 
+    initialHeading = drivetrain.heading(DEGREES)
+    distanceThreshold = 150
+    pointA  = 1
+    pointB = 1
+    angleA  = 1
+    angleB = 1
+
+    
+    # Scan for the two positions
+    while (drivetrain.turn_for(RIGHT, 90)):
+        pointA = range_finder_a.distance(MM)
+        angleA = drivetrain.heading(DEGREES)
+        if (pointA >= distanceThreshold): 
+            brain.screen.print("A Wall has been Detected!")
+            brain.screen.next_row() 
+            break
+        else: 
+            brain.screen.print("No wall detected!")
+            brain.screen.next_row()
+    drivetrain.turn_to_heading(initialHeading)
+    while (drivetrain.turn_for(LEFT, 90)):
+        pointB = range_finder_a.distance(MM)
+        angleB = drivetrain.heading(DEGREES)
+        if (pointB >= distanceThreshold): 
+            brain.screen.print("A Wall has been Detected!")
+            brain.screen.next_row() 
+            break
+        else: 
+            brain.screen.print("No wall detected!")
+            brain.screen.next_row()
+    
+    # Calculate the coodinates from the scanned points
+    pointAx = ((((pointA) * math.cos(angleA)) + 0)/ 180.0 * math.pi)
+    pointAy = ((((pointA) * math.sin(angleA)) + 0)/ 180.0 * math.pi)
+    pointBx = ((((pointB) * math.cos(angleB)) + 0)/ 180.0 * math.pi)
+    pointBy = ((((pointB) * math.sin(angleB)) + 0)/ 180.0 * math.pi)
+    midPointX = ((pointAx-pointBx)/2)+pointBx
+    midPointY = ((pointAy-pointBy)/2)+pointBy
+
+    # Drive to the midppint
+    drivetrain.turn_to_heading (initialHeading+90)
+    drivetrain.drive_for(FORWARD, midPointX)
+    drivetrain.turn_to_heading (initialHeading)
+    drivetrain.drive_for(FORWARD, midPointY)
+
+
 def when_started1():
     global sensingDistance, drivingDistance, totalTravel, TravelLength
     drivingDistance = 0
     totalTravel = [] # For the totalTravel array, 0 means driving, 1 means turning (negative value represents turning left, while a positive value representss turing right), 2 means decision point
+    
     while (True == True):
       sensingDistance = range_finder_a.distance(MM)
       brain.screen.print(sensingDistance)
       brain.screen.next_row()
       drivingDistance = 0
       if(sensingDistance > 150 or drivingDistance > 200):
-          drivetrain.drive(FORWARD)
-          drivingDistance += 1
+          drivetrain.drive_for(FORWARD, 200)
+          midpointDetectAndDrive()
+          drivingDistance += 200
       else:
           drivetrain.stop()
           # Append driving distance to the totalTravel array
@@ -75,7 +132,7 @@ def when_started1():
               totalTravel.append(addItem)
               addItem.clear()
           else: #turn around and return to last decision point
-              TravelLength = totalTravel.count()
+              TravelLength = totalTravel.count
               while (TravelLength != -1):
                 TravelItem = totalTravel[TravelLength]
                 TravelIdentifier = TravelItem [0]
