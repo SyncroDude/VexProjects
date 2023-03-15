5370cb2da94a2c31cb932ed889588e064e7ece60
Shea Stanley <sheahstanley@comcast.net>
2023-03-15 13:55:13 -0400

    Major difficulties!
    I tried my best to get this program to work, and I got so close. I started to run into an issue where the turning became unreliable and the robot would veer off course. It would compound heavily. From what I can assume, the robot would be off course by about 5 degrees after each turn, which caused major issues with proper driving. I also came across an issue with Specular reflection, where any angle over 45 degrees would give bad data. I tried to work around this with centering myself between two walls every 70cm (ish, seems like everything is an approximation when it comes to this robot), but would have a hard time reading the occasional wall even if it was less than 45 degrees relative to the sensor. Very frustrating stuff.

--- Python/MazeSolver_Python_V02/src/main.py
+++ Python/MazeSolver_Python_V02/src/main.py
@@ -1,3 +1,17 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
+
+# Brain should be defined by default
+brain=Brain()
+
+# Robot configuration code
+motor_8 = Motor(Ports.PORT8, GearSetting.RATIO_18_1, False)
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+#endregion VEXcode Generated Robot Configuration
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
@@ -38,79 +52,66 @@ def calibrate_drivetrain():
     brain.screen.clear_screen()
     brain.screen.set_cursor(1, 1)
 
-def midpointDetectAndDrive():
-    # turn to the leftmost heading, then turn from the left until sensed distance is greater than 100. Once achiived, turn to the rightmost heading and do the same.
-    # Take the two distances collected, give them coodrdinates based on your current location (0,0), then interpret output coodrinates so the robot drives to the midpoint of that line.
-    # Version one, drive to the coordinates using components (easy)
-    # Version two, drive to the coodinates by taking the shortest path (harder, with more hurtles to overcome. How do I know the previous heading?)
-    
-    brain.screen.print("Collecting Data for midpoint scan")
-    brain.screen.next_row() 
-    initialHeading = drivetrain.heading(DEGREES)
-    distanceThreshold = 195
-    pointA  = 1
-    pointB = 1
-    angleA  = 1
-    angleB = 1
-
-    
-    # Scan for the two positions
-
-    drivetrain.turn_for(RIGHT, 45)
-    pointA = range_finder_a.distance(MM)
-    angleA = drivetrain.heading(DEGREES)
-    if (pointA >= distanceThreshold < 350): 
-        brain.screen.print("A Wall has been Detected!")
-        brain.screen.next_row() 
-        
-    else: 
-        brain.screen.print("No wall detected!")
-        brain.screen.next_row()
-    drivetrain.turn_to_heading(initialHeading)
-    drivetrain.turn_for(LEFT, 45)
-    pointB = range_finder_a.distance(MM)
-    angleB = drivetrain.heading(DEGREES)
-    if (pointB >= distanceThreshold < 350): 
-        brain.screen.print("A Wall has been Detected!")
-        brain.screen.next_row() 
-    else: 
-        brain.screen.print("No wall detected!")
-        brain.screen.next_row()
-    
-    # Calculate the coodinates from the scanned points
-    pointAx = ((pointA) * math.cos(angleA)) 
-    pointAy = ((pointA) * math.sin(angleA))
-    pointBx = ((pointB) * math.cos(angleB))
-    pointBy = ((pointB) * math.sin(angleB))
-    midPointX = ((pointAx-pointBx)/2)+pointBx
-    midPointY = ((pointAy-pointBy)/2)+pointBy
-
-    # Drive to the midppint
-    drivetrain.turn_to_heading (initialHeading+90)
-    drivetrain.drive_for(FORWARD, midPointX)
-    drivetrain.turn_to_heading (initialHeading)
-    drivetrain.drive_for(FORWARD, midPointY)
-
-
 
 def when_started1():
     global sensingDistance, drivingDistance, totalTravel, TravelLength
     drivingDistance = 0
     totalTravel = [] # For the totalTravel array, 0 means driving, 1 means turning (negative value represents turning left, while a positive value representss turing right), 2 means decision point
     motor_8.spin_for(REVERSE, 10, DEGREES)
-    while (True == True):
+    while (True == True): 
       sensingDistance = range_finder_a.distance(MM)
       brain.screen.print(sensingDistance)
       brain.screen.next_row()
       drivingDistance = 0
-      if(sensingDistance > 150 or drivingDistance > 200):
-          midpointDetectAndDrive()
+      if(sensingDistance > 100):
+          drivingDistance = 0
+          while(range_finder_a.distance(MM) > 100):
+            drivetrain.drive_for(FORWARD, 10)
+            brain.screen.print(range_finder_a.distance(MM))
+            brain.screen.print(" ,")
+            brain.screen.print(drivingDistance)
+            brain.screen.next_row()
+            drivingDistance = drivingDistance + 10
+            if (drivingDistance >= 70):
+                drivetrain.turn_for(RIGHT, 90, DEGREES)
+                optionA = range_finder_a.distance(MM)
+                brain.screen.print(optionA)
+                brain.screen.next_row()
+                drivetrain.turn_for(LEFT, 180, DEGREES)
+                optionB = range_finder_a.distance(MM)
+                brain.screen.print(optionB)
+                brain.screen.next_row()
+                drivetrain.turn_for(RIGHT, 90, DEGREES)
+                if (optionA>0 and optionB>0):
+                    brain.screen.print("Centering Function!")
+                    brain.screen.next_row()
+                    centering = (50+optionA+optionB)/2
+                    brain.screen.print(centering)
+                    brain.screen.next_row()
+                    drivetrain.turn_for(RIGHT, 90, DEGREES)
+                    drivetrain.drive_for(FORWARD, centering-optionA, MM)
+                    drivetrain.turn_for(LEFT, 90, DEGREES)
+                    drivingDistance = int(000)
+                    brain.screen.clear_screen()
+                    brain.screen.set_cursor(1,1)
+                    brain.screen.print("DISTANCE VALUE = ")
+                    brain.screen.print(drivingDistance)
+                    brain.screen.next_row()
+
+
       else:
           drivetrain.stop()
           # Append driving distance to the totalTravel array
           addItem = [0,drivingDistance]
+          brain.screen.print("Array Items: ")
+          brain.screen.print(addItem[0])
+          brain.screen.print(",")
+          brain.screen.print(addItem[0])
+          brain.screen.next_row()
           totalTravel.append(addItem)
           addItem.clear()
+          brain.screen.print("Option Testing")
+          brain.screen.next_row()
           drivetrain.turn_for(RIGHT, 90, DEGREES)
           optionA = range_finder_a.distance(MM)
           drivetrain.turn_for(LEFT, 180, DEGREES)
@@ -132,9 +133,15 @@ def when_started1():
               totalTravel.append(addItem)
               addItem.clear()
           else: #turn around and return to last decision point
-              TravelLength = totalTravel.count
-              while (TravelLength != -1):
-                TravelItem = totalTravel[TravelLength]
+              TravelLength = len(totalTravel)
+              brain.screen.print("Array Length: ")
+              brain.screen.print(TravelLength)
+              brain.screen.next_row()
+              while (TravelLength > 0):
+                TravelItem = totalTravel[TravelLength-1]
+                brain.screen.print("Array Length: ")
+                brain.screen.print(len(TravelItem))
+                brain.screen.next_row()
                 TravelIdentifier = TravelItem [0]
                 TravelValue = [1]
                 if (TravelIdentifier == 0 ):
